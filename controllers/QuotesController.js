e4c575703bf69ee3ff87797e38e62737d0cba83f
NdekoCode <arickbulakali@gmail.com>
2022-10-02 16:10:13 +0200

    validation on post data

--- controllers/QuotesController.js
+++ controllers/QuotesController.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { QuoteModel } from "../models/QuoteModel";
+import { isEmpty, isEmptyObject } from "../utils/utils";
 export const router = Router();
 // On partira de la racine du site,cette route ici "/" est equivalent à http://localhost:3500/quotes
 router.get("/", (req, res) => {
@@ -8,21 +9,51 @@ router.get("/", (req, res) => {
       console.log("Pas de quotes trouver ");
       return error;
     }
-    console.log(posts);
     res.json(posts);
   });
 });
 //  est equivalent à http://localhost:3500/quotes en POST
 router.post("/", (req, res) => {
-  const quotesRecord = new QuoteModel({
-    quote: req.body.quote,
-    author: req.body.author,
-  });
-  quotesRecord.save((err, data) => {
-    if (err) {
-      console.log("Erreur lors de l'insertion des quotes");
-      return err;
+  if (!isEmptyObject(req.body)) {
+    const { author, quote } = req.body;
+    if (isEmpty(author) || isEmpty(quote)) {
+      res.status(400);
+      return res.send({
+        alert: "Veuiller remplir tous les champs",
+        type: "danger",
+      });
     }
-    res.send(data);
-  });
+    QuoteModel.exists({ quote }, (err, data) => {
+      if (err) {
+        res.status(400);
+        return res.send({
+          alert: "Erreur lors de la verification des données",
+          type: "danger",
+        });
+      }
+      if (data) {
+        res.status(200);
+        return res.send({
+          alert: "La citation existe déja !",
+          type: "infos",
+        });
+      } else {
+        const quotesRecord = new QuoteModel({ quote, author });
+        quotesRecord.save((err, data) => {
+          if (err) {
+            console.log("Erreur lors de l'insertion des quotes");
+            throw err;
+          }
+          res.status(200);
+          return res.send(data);
+        });
+      }
+    });
+  } else {
+    res.status(400);
+    return res.send({
+      alert: "Les données demander ne sont pas disponible, ressayer",
+      type: "danger",
+    });
+  }
 });
