bdd8168fdc03f40c31feb54f936158d14ee4c8c8
NdekoCode <arickbulakali@gmail.com>
2022-10-02 12:45:50 +0200

    feat: add main controllers

--- models/QuoteModel.js
+++ models/QuoteModel.js
@@ -1,4 +1,4 @@
-import mongoose, { model, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 const QuoteSchema = new Schema({
   author: {
     type: String,
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { httpServerConfig } from "./config/httpConfig";
 import { dbConfig } from "./config/dbConfig";
+import { router } from "./controllers/PostsController";
 const app = express();
 httpServerConfig(app);
 dbConfig();
-app.get("/", (req, res) => {
-  res.end("hello");
-});
+const postsRoutes = router;
+
+// MIDDLEWARE
+// A chaque fois que l'on va appeler la route "/" alors appele postsRoutes
+app.use("/", postsRoutes);
+
+// ON fait ecouter notre serveur sur le port 3500
 app.listen(process.env.PORT || 3500, () => {
   console.log("Server started... on http://localhost:3500");
 });
