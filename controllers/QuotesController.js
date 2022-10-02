334adc3d09302d7193299619127c0ae9f3bfac1a
NdekoCode <arickbulakali@gmail.com>
2022-10-02 14:15:21 +0200

    feat: post data in db

--- controllers/QuotesController.js
+++ controllers/QuotesController.js
@@ -1,11 +1,28 @@
 import { Router } from "express";
 import { QuoteModel } from "../models/QuoteModel";
 export const router = Router();
-// On partira de la racine du site,cette route ici "/" est equivalent à http://localhost:3500/posts
+// On partira de la racine du site,cette route ici "/" est equivalent à http://localhost:3500/quotes
 router.get("/", (req, res) => {
   QuoteModel.find((error, posts) => {
-    if (error) return error;
+    if (error) {
+      console.log("Pas de quotes trouver ");
+      return error;
+    }
     console.log(posts);
     res.json(posts);
+  });
+});
+//  est equivalent à http://localhost:3500/quotes en POST
+router.post("/", (req, res) => {
+  const quotesRecord = new QuoteModel({
+    quote: req.body.quote,
+    author: req.body.author,
+  });
+  quotesRecord.save((err, data) => {
+    if (err) {
+      console.log("Erreur lors de l'insertion des quotes");
+      return err;
+    }
+    res.send(data);
   });
 });
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { httpServerConfig } from "./config/httpConfig";
 import { dbConfig } from "./config/dbConfig";
 import { router } from "./controllers/QuotesController";
@@ -7,8 +8,11 @@ httpServerConfig(app);
 dbConfig();
 const quotesRouter = router;
 // MIDDLEWARE
-// QUand l'utilisateur va vouloir aller sur l'url "/quotes" alors appele postsRoutes
 
+// body-parser pour avoir le requete dans le corps de notre application
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // Cela permet de parser du json, il est equivalent à JSON.parse application/json parser
+// QUand l'utilisateur va vouloir aller sur l'url "/quotes" alors appele postsRoutes
 app.use("/quotes", quotesRouter);
 // ON fait ecouter notre serveur sur le port 3500
 app.listen(process.env.PORT || 3500, () => {
