0777ab2fcea4a350a8b1f7095092774a4f70f88d
NdekoCode <arickbulakali@gmail.com>
2022-10-03 12:03:19 +0200

    feat: PUT method for quote API

--- controllers/QuotesController.js
+++ controllers/QuotesController.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { QuoteModel } from "../models/QuoteModel";
 import { isEmpty, isEmptyObject } from "../utils/utils";
+import { isValidObjectId } from "mongoose";
+import slugify from "slugify";
 export const router = Router();
 // On partira de la racine du site,cette route ici "/" est equivalent à http://localhost:3500/quotes
 router.get("/", (req, res) => {
@@ -14,9 +16,9 @@ router.get("/", (req, res) => {
     res.json(posts);
   });
 });
-//  est equivalent à http://localhost:3500/quotes en POST
+// POST: est equivalent à http://localhost:3500/quotes en POST
 // On veut inserer une citation dans la base de données
-router.post("/", (req, res) => {
+router.post("/store", (req, res) => {
   // Si le corps de la requete n'est pas vide
   if (!isEmptyObject(req.body)) {
     // On recupère les informations qui nous interesse dans l'API
@@ -32,37 +34,44 @@ router.post("/", (req, res) => {
       });
     }
     // On verifie si la citation existe déjà dans la base de données
-    QuoteModel.exists({ quote }, (err, data) => {
-      if (err) {
-        // S'il y a une erreur on retourne un status 400
-        res.status(400);
-        // On envois un alert
-        return res.send({
-          statusCode: 400,
-          alert: "Erreur lors de la verification des données",
-          type: "danger",
-        });
+    QuoteModel.exists(
+      { quote, slug: slugify(quote.substring(0, 25)) },
+      (err, data) => {
+        if (err) {
+          // S'il y a une erreur on retourne un status 400
+          res.status(400);
+          // On envois un alert
+          return res.send({
+            statusCode: 400,
+            alert: "Erreur lors de la verification des données",
+            type: "danger",
+          });
+        }
+        // On verifie si dans les resultat on constate que les données oxiste alors on envois une une alert
+        if (data) {
+          res.status(200);
+          return res.send({
+            statusCode: 200,
+            alert: "La citation existe déja !",
+            type: "infos",
+          });
+        } else {
+          // On insere les données dans la base de données
+          const quotesRecord = new QuoteModel({
+            quote,
+            author,
+            slug: slugify(quote.substring(0, 25)),
+          });
+          quotesRecord.save((err, data) => {
+            if (err) {
+              console.log("Erreur lors de l'insertion des quotes");
+              throw err;
+            }
+            return res.status(200).send(data);
+          });
+        }
       }
-      // On verifie si dans les resultat on constate que les données oxiste alors on envois une une alert
-      if (data) {
-        res.status(200);
-        return res.send({
-          statusCode: 200,
-          alert: "La citation existe déja !",
-          type: "infos",
-        });
-      } else {
-        // On insere les données dans la base de données
-        const quotesRecord = new QuoteModel({ quote, author });
-        quotesRecord.save((err, data) => {
-          if (err) {
-            console.log("Erreur lors de l'insertion des quotes");
-            throw err;
-          }
-          return res.status(200).send(data);
-        });
-      }
-    });
+    );
   } else {
     res.status(400);
     return res.send({
@@ -70,4 +79,48 @@ router.post("/", (req, res) => {
       type: "danger",
     });
   }
+});
+//UPDATE: On veut maintenant modifier des données
+router.put("/update/:id", (req, res) => {
+  // On verifie d'abord si la citation que l'on veut modifier existe bel et bien dans la base de données
+  // Si l'identifiant n'est pas connus alors on emmet une alert
+  if (!isValidObjectId(req.params.id) || isEmptyObject(req.body)) {
+    return res.status(400).send({
+      statusCode: 400,
+      alert: `Données inconnus, verifier vos informations ${req.params.id}`,
+      type: "danger",
+    });
+  }
+
+  const { author, quote } = req.body;
+  if (isEmpty(author) || isEmpty(quote)) {
+    // On emmet une requete 400
+    // On envois une alert
+    return res.status(400).send({
+      statusCode: 400,
+      alert: "Veuiller remplir tous les champs",
+      type: "danger",
+    });
+  }
+  const updateQuoteRecord = {
+    author,
+    slug: slugify(quote.substring(0, 25)),
+    quote,
+    updated_at: Date.now(),
+  };
+  QuoteModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updateQuoteRecord },
+    { new: true },
+    (error, docs) => {
+      if (error) {
+        return res.status(400).send({
+          statusCode: 400,
+          alert: "Une erreur est survenus lors de la modification des données",
+          type: "danger",
+        });
+      }
+      return res.send(docs);
+    }
+  );
 });
